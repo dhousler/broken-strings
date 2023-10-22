88a59e9d2a47a6f95cb64df2f8c29bdd80d4443c
dhousler <unwariedbroom@yahoo.com>
2023-10-22 19:08:52 +0100

    Update normalised_counts.sh
    fix

--- bin/normalised_counts.sh
+++ bin/normalised_counts.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
+# TODO make this smarter so that the files are placed the correct order, workflow manager should handle but could be explicit currently implicit
+
 # Check if exactly two file arguments are provided
-
-# NOTES:
-# file1 will be original raw files, file2 will be filtered intersected
-# As workflow manager handles order, no need to define specifically here, could make smarter
-# decided on bash over python as very fast to count and calculate
-
 if [ "$#" -ne 2 ]; then
     echo "Usage: $0 <file1> <file2>"
     exit 1
@@ -38,6 +34,9 @@ count_raw_1000=$(echo "scale=3; $count_raw/1000" | bc) #$((count_raw / 1000))
 normalised_counts=$(echo "scale=3; ($count_intercept / $count_raw_1000) " | bc)
 
 # Count to integer - round to nearest 1
+#int_normalised_counts=$(echo "scale=0; ($count_intercept / $count_raw_1000) " | bc -l)
+#int_normalised_counts=$(awk -v num="$normalised_counts" 'BEGIN { rounded = int(num + 0.5); printf("%.0f\n", rounded) }')
+#int_normalised_counts=$(awk -v num="$normalised_counts" 'BEGIN { rounded = int(num + 0.5); if (rounded < 0.5) rounded = 0.5; printf("%.2f\n", rounded) }')
 int_normalised_counts=$(awk -v num="$normalised_counts" 'BEGIN {
   if (num == 0) {
     rounded = 0;
@@ -49,6 +48,5 @@ int_normalised_counts=$(awk -v num="$normalised_counts" 'BEGIN {
 }')
 
 # Create a new file (e.g., counts.txt) with headers and all counts
-#NOTE: Header is defined in the workflow params so that headers can be readily defined
+#echo -e "sample\traw_counts\tintercept_counts\traw_div_1000\tnormalised_counts\tint_normalised_count_int" > $sample_name.counts.txt # header removed for collation
 echo -e "$sample_name\t$count_raw\t$count_intercept\t$count_raw_1000\t$normalised_counts\t$int_normalised_counts" >> $sample_name.counts.txt
-
